ef9791fcfbca5cc072b716f3147b7971452f1cf2
Anthonijn <0897523@hr.nl>
2014-12-09 01:40:59 +0100

    kranen bewegen
    De player1 kraan kan bewegen, maar de computerkraan natuurlijk niet,
    omdat die zelf moet bewegen

--- Minigame2/Background.java
+++ Minigame2/Background.java
@@ -15,10 +15,11 @@ public class Background extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1500, 1000, 1);
         // Create new Ships to load containers on to.
-        addObject(new Ship(), 10, 10);
-        addObject(new Ship2(), 10, 10);
+        addObject(new Ship(), 1131, 468);
+        addObject(new Ship2(), 447, 471);
         // Create new Crane to load containers from Wharf to Ship.
-        addObject(new Crane(), 10, 10);
+        addObject(new Crane(), 173, 446);
+        addObject(new computerCrane(), 988, 446);
         // Create container to be shipped from Wharf to Ship.
         addObject(new Container(), 10, 10);
         // Create the Wharfs on which the Containers will be stacked.
--- Minigame2/computerCrane.java
+++ Minigame2/computerCrane.java
@@ -1,15 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class crane2 here.
+ * Write a description of class computerCrane here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class crane2 extends Actor
+public class computerCrane extends Actor
 {
     /**
-     * Act - do whatever the crane2 wants to do. This method is called whenever
+     * Act - do whatever the computerCrane wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
