925589555d40600312847fec1cab9b4c995f0509
daphne van schaik <0852893@hr.nl>
2014-12-17 11:21:58 +0100

    jump

--- Minigame4/Player.java
+++ Minigame4/Player.java
@@ -10,6 +10,8 @@ public class Player extends Actor
 {
     /**Speed of Player*/
     private int speed = 5;
+    private int ySpeed;
+    private int apexTimer;
     /**
      * Act - do whatever the Player wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -24,5 +26,32 @@ public class Player extends Actor
         {
             move (speed); 
         }
-    }    
+        
+        int groundLevel = getWorld().getHeight() - getImage().getHeight()/2;
+        boolean onGround = (getY() == groundLevel);
+        if (!onGround) // in middle of jump
+        {
+            if (ySpeed == 0 && apexTimer > 0) apexTimer--; // run apex timer
+            if (ySpeed == 0 && apexTimer > 0) return; // apex timer still running
+            ySpeed++; // adds gravity effect
+            setLocation(getX(), getY()+ySpeed); // fall (rising slower or falling faster)
+            if (getY()>=groundLevel) // has landed (reached ground level)
+            {
+                setLocation(getX(), groundLevel); // set on ground
+                Greenfoot.getKey(); // clears any key pressed during jump
+           }
+        }
+        else // on ground
+        {
+            if ("space".equals(Greenfoot.getKey())) // jump key detected
+            {
+                ySpeed = -15; // add jump speed
+                setLocation(getX(), getY()+ySpeed); // leave ground
+                apexTimer = 100;  // set apex timer (adjust value to suit)
+            }
+        }
+    
+    }
+    
+    
 }
