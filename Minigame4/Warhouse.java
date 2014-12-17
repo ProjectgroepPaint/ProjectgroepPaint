c49afd9b6e6a2d5fc25164d0022946ae959d055f
daphne van schaik <0852893@hr.nl>
2014-12-17 10:56:13 +0100

    background m4

---
+++ Minigame4/Warhouse.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Warhouse here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Warhouse extends World
+{
+
+    /**
+     * Constructor for objects of class Warhouse.
+     * 
+     */
+    public Warhouse()
+    {    
+        super(800, 600, 1); 
+        
+    }
+}
