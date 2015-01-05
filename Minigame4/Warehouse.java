75b07f6e5f0bedeaf66d7a7a454f47c7de019fe1
Kimberley de Jong <k95dejong@gmail.com>
2015-01-05 21:16:20 +0100

    nieuwe map 4

--- Minigame4/Warehouse.java
+++ Minigame4/Warehouse.java
@@ -12,6 +12,13 @@ public class Warehouse extends World
     {    
         super(800, 600, 1); 
         addObject (new Player(), 100, 570);
-        addObject (new containerSide(), 400, 560);
+        addObject (new containerSide(), 442, 562);
+        addObject (new containerSide2(), 200,572);
+        addObject (new containerSide3(), 650,553);
+        
+        addObject (new containerFront(), 200,572);
+        addObject (new containerFront2(), 800,572);
+        addObject (new crate(), 233,515);
+        addObject (new radioactiveBarrel(), 700,485);
     }
 }
