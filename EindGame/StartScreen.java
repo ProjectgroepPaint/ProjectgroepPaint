e92ccc2c3c9e699e3f598e93304b9021f4a68006
Kimberley de Jong <k95dejong@gmail.com>
2015-01-20 14:19:35 +0100

    menu anders

--- EindGame/StartScreen.java
+++ EindGame/StartScreen.java
@@ -5,7 +5,7 @@ public class StartScreen extends World
     public StartScreen()
     {
         // Create a new world with 1920x1080 cells with a cell size of 1x1 pixels.
-        super(1920, 1080, 1);
+        super(1900, 950, 1);
     }
     public void act()
     {
